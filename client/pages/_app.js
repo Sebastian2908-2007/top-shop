ce1b9e33c4d64183633648bcebcee316a4b2990b
Sebabin <sebastiancrespin279@gmail.com>
2022-11-03 20:29:28 -0600

    I have set up the global state functionality and all of my actions

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,15 @@
 import { ApolloProvider } from '@apollo/client'
 import { useApollo } from '../lib/apolloClient'
+import { StoreProvider } from '../utils/Globalstate';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
 const apolloClient = useApollo(pageProps);
  return( 
    <ApolloProvider client={apolloClient}>
+    <StoreProvider>
     <Component {...pageProps} />
+    </StoreProvider>
   </ApolloProvider>
  );
 }
