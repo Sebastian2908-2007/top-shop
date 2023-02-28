ec1c88cf304e969a4c7694ade0b63393c66e838e
Sebabin <sebastiancrespin279@gmail.com>
2023-02-28 00:38:44 -0700

    re added what I just took away

--- client/lib/apolloClient.js
+++ client/lib/apolloClient.js
@@ -27,7 +27,7 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
  */
 const httpLink = new HttpLink({
   uri: process.env.NEXT_PUBLIC_APOLLO_URI, // Server URL (must be absolute)
- // Additional fetch() options like `credentials` or `headers`
+  credentials: 'include',// Additional fetch() options like `credentials` or `headers`
 })
 /*const schemaLink = new SchemaLink({schema});*/
 
